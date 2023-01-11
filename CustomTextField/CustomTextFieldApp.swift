e309ec0c52bb3dd58f36587f7c5adb8f5534b95f
abdelrahmanTalaat <abdelrahman_adm@outlook.com>
2023-01-11 23:53:39 +0200

    Initial Commit

---
+++ CustomTextField/CustomTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTextFieldApp.swift
+//  CustomTextField
+//
+//  Created by Abdelrahman Talaat on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
