1546c15cf6d54e1e1b209eec31768a7725ca9645
abdelrahmanTalaat <abdelrahman_adm@outlook.com>
2023-01-12 00:18:09 +0200

    Add floating border label TextField example to ContentView

--- CustomTextField/ContentView.swift
+++ CustomTextField/ContentView.swift
@@ -13,14 +13,28 @@ struct ContentView: View {
 	
     var body: some View {
         VStack {
-			Text("Floating Label TextField")
-				.font(.title2)
-				.fontWeight(.bold)
-				.foregroundColor(.black)
+			Group {
+				Text("Floating Label TextField")
+					.font(.title2)
+					.fontWeight(.bold)
 
-			FloatingLabelTextField(placeholder: "First Name", text: self.$firstName)
-			FloatingLabelTextField(placeholder: "Last Name", text: self.$lastName)
+				FloatingLabelTextField(placeholder: "First Name", text: self.$firstName)
+				FloatingLabelTextField(placeholder: "Last Name", text: self.$lastName)
+			}
+			
+			Divider()
+			
+			Group {
+				Text("Floating Border Label TextField")
+					.font(.title2)
+					.fontWeight(.bold)
+
+				FloatingBorderLabelTextField(placeholder: "First Name", text: self.$firstName)
+				FloatingBorderLabelTextField(placeholder: "Last Name", text: self.$lastName)
+			}
+			.padding(.vertical, 5)
         }
+		.foregroundColor(.black)
         .padding()
     }
 }
